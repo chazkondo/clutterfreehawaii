4d555651e94f79b9a58fd7034d045fb9222771b5
Chaz Kondo <chazkondo@gmail.com>
2020-10-13 15:31:33 -1000

    Abstract away light button into its own component to reduce rerenders

---
+++ src/components/Buttons/LightSwitch/LightSwitch.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+
+//font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import "../../../utils/fontawesome"
+
+import classnames from "classnames"
+
+export default function LightSwitch({ backgroundDark, setBackgroundDark }) {
+
+    const [jumpEffect, toggleJumpEffect] = React.useState(``)
+    const [nightModeClicked, setNightModeClicked] = React.useState(false)
+
+    function toggleMode() {
+        if (!nightModeClicked) {
+            setNightModeClicked(true)
+        }
+        return setBackgroundDark(current => !current)
+    }
+
+    React.useEffect(() => {
+        let initialTimeout
+        let setJumpInterval
+        function turnOnJumpEffect() {
+            toggleJumpEffect(`jump`)
+            return (initialTimeout = setTimeout(
+                () => toggleJumpEffect(``),
+                3000
+            ))
+        }
+        if (!nightModeClicked) {
+            turnOnJumpEffect()
+        }
+        if (nightModeClicked) {
+            clearTimeout(initialTimeout)
+            return clearTimeout(setJumpInterval)
+        }
+        return () => {
+            clearTimeout(initialTimeout)
+            clearTimeout(setJumpInterval)
+        }
+    }, [])
+
+    return (
+        <span
+            className="lightButton"
+            onClick={() => toggleMode()}
+            style={{
+                cursor: `pointer`,
+                bottom: `10%`,
+                right: `3%`,
+                position: `fixed`,
+                zIndex: 100000,
+                color: `rgba(255,255,255,0.3)`,
+                // background: `black`,
+            }}
+        >
+            {backgroundDark ? (
+                <span
+                    className={classnames(jumpEffect)}
+                    style={{
+                        display: `grid`,
+                        gridTemplateColumns: `1fr`,
+                        gridTemplateRows: `1fr`,
+                        gridTemplateAreas: `'overlap'`,
+                        margin: 0,
+                        gridGap: 0,
+                        padding: 0,
+                    }}
+                >
+                    <FontAwesomeIcon
+                        className="overlap"
+                        style={{
+                            filter: `drop-shadow(1px 1px 1px #000000)`,
+                            transform: `rotate(-10deg)`,
+                        }}
+                        size={`lg`}
+                        icon={[`fas`, `sun`]}
+                        color="black"
+                        opacity={0.5}
+                    />
+                    <FontAwesomeIcon
+                        className="overlap"
+                        style={{
+                            position: `relative`,
+                            right: `10%`,
+                            transform: `rotate(-10deg)`,
+                        }}
+                        size={`lg`}
+                        icon={[`fas`, `sun`]}
+                        color="rgba(255,255,255,0.6)"
+                    />
+                </span>
+            ) : (
+                    <span
+                        className={classnames(jumpEffect)}
+                        style={{
+                            display: `grid`,
+                            gridTemplateColumns: `1fr`,
+                            gridTemplateRows: `1fr`,
+                            gridTemplateAreas: `'overlap'`,
+                            margin: 0,
+                            gridGap: 0,
+                            padding: 0,
+                        }}
+                    >
+                        <FontAwesomeIcon
+                            className="overlap"
+                            style={{
+                                filter: `drop-shadow(1px 1px 1px #000000)`,
+                                // transform: `scaleX(-1)`,
+                                transform: `rotate(-10deg)`,
+                            }}
+                            size={`lg`}
+                            icon={[`fas`, `moon`]}
+                            color="black"
+                            opacity={0.5}
+                        />
+                        <FontAwesomeIcon
+                            className="overlap"
+                            style={{
+                                filter: `drop-shadow(1px 1px 1px #ffffff)`,
+                                position: `relative`,
+                                right: `10%`,
+                                transform: `rotate(-10deg)`,
+                                // transform: `scaleX(-1)`,
+                            }}
+                            size={`lg`}
+                            icon={[`fas`, `moon`]}
+                            color="rgba(255,255,255,0.4)"
+                        />
+                    </span>
+                )}
+        </span>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,59 +10,21 @@ import Section3 from "../components/Sections/Section3"
 import Section4 from "../components/Sections/Section4"
 import Section5 from "../components/Sections/Section5"
 
+import LightSwitch from "../components/Buttons/LightSwitch/LightSwitch.jsx"
+
 //css
 import "../assets/css/bootstrap.min.css"
 import "../assets/scss/paper-kit.scss?v=1.2.0"
 import "../assets/demo/demo.css?v=1.2.0"
 
-//font awesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import "../utils/fontawesome"
-
-import classnames from "classnames"
-
 function Index() {
     const [borderVisible, setBorderVisible] = React.useState(false)
     const [backgroundDark, setBackgroundDark] = React.useState(false)
-    const [nightModeClicked, setNightModeClicked] = React.useState(false)
-    // const [isLoading, setIsLoading] = React.useState(true)
-    const [jumpEffect, toggleJumpEffect] = React.useState(``)
-
-    function toggleMode() {
-        if (!nightModeClicked) {
-            setNightModeClicked(true)
-        }
-        return setBackgroundDark(current => !current)
-    }
-
-    React.useEffect(() => {
-        let initialTimeout
-        let setJumpInterval
-        function turnOnJumpEffect() {
-            toggleJumpEffect(`jump`)
-            return (initialTimeout = setTimeout(
-                () => toggleJumpEffect(``),
-                3000
-            ))
-        }
-        if (!nightModeClicked) {
-            turnOnJumpEffect()
-        }
-        if (nightModeClicked) {
-            clearTimeout(initialTimeout)
-            return clearTimeout(setJumpInterval)
-        }
-        return () => {
-            clearTimeout(initialTimeout)
-            clearTimeout(setJumpInterval)
-        }
-    }, [])
 
     React.useLayoutEffect(() => {
         const hours = new Date().getHours()
         const isDayTime = hours > 6 && hours < 18
         setBackgroundDark(!isDayTime)
-        // setIsLoading(false)
     }, [])
 
     return (
@@ -81,97 +43,11 @@ function Index() {
                 <Section3 backgroundDark={backgroundDark} />
                 <Section4 backgroundDark={backgroundDark} />
                 {/* <Section5 backgroundDark={backgroundDark} /> */}
-                <span
-                    className="lightButton"
-                    onClick={() => toggleMode()}
-                    style={{
-                        cursor: `pointer`,
-                        bottom: `10%`,
-                        right: `3%`,
-                        position: `fixed`,
-                        zIndex: 100000,
-                        color: `rgba(255,255,255,0.3)`,
-                        // background: `black`,
-                    }}
-                >
-                    {backgroundDark ? (
-                        <span
-                            className={classnames(jumpEffect)}
-                            style={{
-                                display: `grid`,
-                                gridTemplateColumns: `1fr`,
-                                gridTemplateRows: `1fr`,
-                                gridTemplateAreas: `'overlap'`,
-                                margin: 0,
-                                gridGap: 0,
-                                padding: 0,
-                            }}
-                        >
-                            <FontAwesomeIcon
-                                className="overlap"
-                                style={{
-                                    filter: `drop-shadow(1px 1px 1px #000000)`,
-                                    transform: `rotate(-10deg)`,
-                                }}
-                                size={`lg`}
-                                icon={[`fas`, `sun`]}
-                                color="black"
-                                opacity={0.5}
-                            />
-                            <FontAwesomeIcon
-                                className="overlap"
-                                style={{
-                                    position: `relative`,
-                                    right: `10%`,
-                                    transform: `rotate(-10deg)`,
-                                }}
-                                size={`lg`}
-                                icon={[`fas`, `sun`]}
-                                color="rgba(255,255,255,0.6)"
-                            />
-                        </span>
-                    ) : (
-                        <span
-                            className={classnames(jumpEffect)}
-                            style={{
-                                display: `grid`,
-                                gridTemplateColumns: `1fr`,
-                                gridTemplateRows: `1fr`,
-                                gridTemplateAreas: `'overlap'`,
-                                margin: 0,
-                                gridGap: 0,
-                                padding: 0,
-                            }}
-                        >
-                            <FontAwesomeIcon
-                                className="overlap"
-                                style={{
-                                    filter: `drop-shadow(1px 1px 1px #000000)`,
-                                    // transform: `scaleX(-1)`,
-                                    transform: `rotate(-10deg)`,
-                                }}
-                                size={`lg`}
-                                icon={[`fas`, `moon`]}
-                                color="black"
-                                opacity={0.5}
-                            />
-                            <FontAwesomeIcon
-                                className="overlap"
-                                style={{
-                                    filter: `drop-shadow(1px 1px 1px #ffffff)`,
-                                    position: `relative`,
-                                    right: `10%`,
-                                    transform: `rotate(-10deg)`,
-                                    // transform: `scaleX(-1)`,
-                                }}
-                                size={`lg`}
-                                icon={[`fas`, `moon`]}
-                                color="rgba(255,255,255,0.4)"
-                            />
-                        </span>
-                    )}
-                </span>
             </div>
+            <LightSwitch
+                backgroundDark={backgroundDark}
+                setBackgroundDark={setBackgroundDark}
+            />
             <Footer backgroundDark={backgroundDark} />
         </>
     )
