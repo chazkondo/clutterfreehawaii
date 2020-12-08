75ed4790b8a7305baec57c1fa9836058f31496e5
Chaz Kondo <chazkondo@gmail.com>
2020-12-08 13:25:50 -1000

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,9 +20,13 @@ import "../assets/demo/demo.css?v=1.2.0"
 // helper
 import useWindowSize from "../components/Helpers/useWindowSize.js"
 
+// helmet
+import { Helmet } from "react-helmet"
+
 function Index() {
     const [backgroundDark, setBackgroundDark] = React.useState(false)
     const size = useWindowSize()
+    const title = `Clutter Free. Zero Stress. Your local hero.`
 
     React.useLayoutEffect(() => {
         const hours = new Date().getHours()
@@ -32,6 +36,40 @@ function Index() {
 
     return (
         <>
+            <Helmet title={title} defer={false}>
+                <title>{title}</title>
+                <link rel="icon" href="/favicon.jpg" />
+                <meta property="og:title" content={title} />
+                <meta
+                    property="og:url"
+                    content="https://clutterfreehawaii.com"
+                />
+                <meta
+                    property="og:image"
+                    content="https://clutterfreehawaii.com/snap.png"
+                />
+                <meta
+                    property="og:description"
+                    content="Your top rated and simple clutter removal service."
+                />
+                <meta property="og:site_name" content="Clutter Free Hawaii" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:title" content={title} />
+                <meta
+                    name="twitter:description"
+                    content="Your top rated and simple clutter removal service."
+                />
+                <meta name="twitter:creator" content="@DevinLonso" />
+                <meta
+                    name="twitter:image"
+                    content="https://clutterfreehawaii.com/snap.png"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, maximum-scale=1"
+                />
+                <link rel="shortcut icon" href="/favicon.jpg" />
+            </Helmet>
             <IndexNavbar showProgressBar backgroundDark={backgroundDark} />
             <IndexHeader backgroundDark={backgroundDark} size={size} />
             <div className="main">
