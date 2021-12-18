eac13931b95d22ad752f95ce97b8cf4dac470d04
Chaz Kondo <chuckeekondo@gmail.com>
2021-12-17 20:26:10 -1000

    fix minor issues

--- src/components/Headers/MyBackgroundImage.jsx
+++ src/components/Headers/MyBackgroundImage.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
+import React from "react"
 
 import { graphql, useStaticQuery } from "gatsby"
 
 import Img from "gatsby-image"
 
 export default function MyBackgroundImage({ backgroundOpacity, setLoaded }) {
-
     const data = useStaticQuery(
         graphql`
             query {
-                static1: file(relativePath: { eq: "creative-commons-zen-large.jpg" }) {
+                static1: file(
+                    relativePath: { eq: "creative-commons-zen-large.jpg" }
+                ) {
                     childImageSharp {
                         fluid(quality: 100, maxWidth: 1920) {
                             ...GatsbyImageSharpFluid_withWebp
                         }
                     }
                 }
-                static2: file(relativePath: { eq: "creative-commons-zen.jpg" }) {
+                static2: file(
+                    relativePath: { eq: "creative-commons-zen.jpg" }
+                ) {
                     childImageSharp {
                         fluid(quality: 100, maxWidth: 1280) {
                             ...GatsbyImageSharpFluid_withWebp
                         }
                     }
                 }
-                static3: file(relativePath: { eq: "creative-commons-zen-small.jpg" }) {
+                static3: file(
+                    relativePath: { eq: "creative-commons-zen-small.jpg" }
+                ) {
                     childImageSharp {
                         fluid(quality: 100, maxWidth: 640) {
                             ...GatsbyImageSharpFluid_withWebp
                         }
                     }
                 }
-                static4: file(relativePath: { eq: "creative-commons-zen-xs.jpg" }) {
+                static4: file(
+                    relativePath: { eq: "creative-commons-zen-xs.jpg" }
+                ) {
                     childImageSharp {
                         fluid(quality: 100, maxWidth: 400) {
                             ...GatsbyImageSharpFluid_withWebp
@@ -44,34 +51,36 @@ export default function MyBackgroundImage({ backgroundOpacity, setLoaded }) {
     const sources = [
         {
             ...data.static3.childImageSharp.fluid,
-            media: `(max-width: 300px)`
+            media: `(max-width: 300px)`,
         },
         {
             ...data.static2.childImageSharp.fluid,
             media: `(min-width: 301px) and (max-width: 999px)`,
-          },
+        },
         {
-          ...data.static1.childImageSharp.fluid,
-          media: `(min-width: 1000px)`,
+            ...data.static1.childImageSharp.fluid,
+            media: `(min-width: 1000px)`,
         },
         // {
         //     ...data.static1.childImageSharp.fluid,
         //     media: `(min-width: 1920px)`,
         //   },
-      ]
+    ]
 
-    return <Img
-        Tag="section"
-        loading="eager"
-        className="page-header"
-        style={{
-            position: "absolute",
-            // backgroundColor: "rgba(255,255,255,1)",
-            opacity: backgroundOpacity,
-            transition: "opacity 3s linear",
-        }}
-        fluid={sources}
-        onLoad={()=>setLoaded(true)}
-        onStartLoad={()=>setLoaded(false)}
-    ></Img>
+    return (
+        <Img
+            Tag="section"
+            loading="eager"
+            className="page-header"
+            style={{
+                position: `absolute`,
+                // backgroundColor: "rgba(255,255,255,1)",
+                opacity: backgroundOpacity,
+                transition: `opacity 3s linear`,
+            }}
+            fluid={sources}
+            onLoad={() => setLoaded(true)}
+            onStartLoad={() => setLoaded(false)}
+        ></Img>
+    )
 }
